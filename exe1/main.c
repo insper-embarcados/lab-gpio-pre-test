cc52d7f6ba677b6651a2298a24ab3f197ad7753a
Rafael Corsi <rafael.corsi@insper.edu.br>
2023-11-25 17:38:46 -0300

    Update main.c

--- exe1/main.c
+++ exe1/main.c
@@ -2,17 +2,19 @@
 #include "pico/stdlib.h"
 
 int main() {
+
   // codigo init pin aqui
-  
+  gpio_init(2);
+  gpio_set_dir(2, GPIO_OUT);
   while (true) {
     printf("LED on\n"); /* nao tirar  */
     // codigo ativa pino aqui 
-    
+    gpio_put(2, 1); 
     sleep_ms(250);
 
     printf("LED off\n"); /* nao tirar  */
     // codigo desliga pino aqui 
-
+    gpio_put(2, 0); 
     sleep_ms(250);
   }
 }
