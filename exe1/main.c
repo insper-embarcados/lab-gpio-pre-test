19f8385700dde06cac3f8798959eaaf20316db60
Rafael Corsi <rafael.corsi@insper.edu.br>
2023-11-25 17:21:08 -0300

    exe1

--- exe1/main.c
+++ exe1/main.c
@@ -1,21 +1,22 @@
-#include "pico/cyw43_arch.h"
-#include "pico/stdlib.h"
 #include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
 int main() {
-  stdio_init_all();
 
   // codigo init pin aqui
-
+  gpio_init(2);
+  gpio_set_dir(2, GPIO_OUT);
+  
   while (true) {
     printf("LED on\n"); /* nao tirar  */
-    // codigo ativa pino aqui
-
+    // codigo ativa pino aqui 
+    gpio_put(2, 1);
     sleep_ms(250);
 
     printf("LED off\n"); /* nao tirar  */
-    // codigo desliga pino aqui
-
+    // codigo desliga pino aqui 
+    gpio_put(2, 0);
     sleep_ms(250);
   }
 }
