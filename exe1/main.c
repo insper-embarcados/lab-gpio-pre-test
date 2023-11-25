79b20e395329b3bd927831d220ae7c5c7a137d4e
Rafael Corsi <rafael.corsi@insper.edu.br>
2023-11-25 17:44:53 -0300

    Update main.c

--- exe1/main.c
+++ exe1/main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_PIN 2
+
 int main() {
 
   // codigo init pin aqui
-  gpio_init(2);
-  gpio_set_dir(2, GPIO_OUT);
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
   while (true) {
     printf("LED on\n"); /* nao tirar  */
     // codigo ativa pino aqui 
-    gpio_put(2, 1); 
+    gpio_put(LED_PIN, 1); 
     sleep_ms(250);
 
     printf("LED off\n"); /* nao tirar  */
     // codigo desliga pino aqui 
-    gpio_put(2, 0); 
+    gpio_put(LED_PIN, 0); 
     sleep_ms(250);
   }
 }
